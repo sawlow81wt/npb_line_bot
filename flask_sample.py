52a0fda2240749f5d513a94ecf4bb4be9890d2bb
sotaro maehara <s.maehara81@icloud.com>
2018-05-02 21:12:39 +0900

    fix callback method name

--- flask_sample.py
+++ flask_sample.py
@@ -26,7 +26,7 @@ handler = WebhookHandler(LINE_CHANNEL_SECRET)
 def ok():
     return "ok!!"
 
-@app.route("/", methods=['POST'])
+@app.route("/callback", methods=['POST'])
 def callbask():
 
     signature = request.headers['X-Line-Signature']
