cc969cee26bc6f174457d99f5d9b6df7eb4f3ea0
sotaro maehara <s.maehara81@icloud.com>
2018-05-02 18:20:56 +0900

    can reply

--- flask_sample.py
+++ flask_sample.py
@@ -2,22 +2,50 @@ from flask import Flask, jsonify, request
 import os
 import json
 import getGameScore
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
 app = Flask(__name__)
 
 
 LINE_ACCESS_TOKEN=os.getenv("LINE_ACCESS_TOKEN")
 LINE_CHANNEL_SECRET=os.getenv("LINE_CHANNEL_SECRET")
 
+line_bot_api = LineBotApi(LINE_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
-@app.route("/", methods=['GET'])
-def post():
-    favor_team = "阪神"
+@app.route("/", methods=['POST'])
+def callbask():
+
+    signature = request.headers['X-Line-Signature']
+
+    body = request.get_data(as_text=True)
+    app.loger.info("Request body: {}".format(body))
+
+    try:
+        handler.handl(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    
+    return 'ok'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    favor_team = event.message.text
     data = getGameScore.get_today_score_list(favor_team)
-    return_data = {idx: {"text":val} for idx, val in enumerate(data)}
-    return jsonify({
-        'status': 'OK',
-        'data': return_data
-    })
+    reply_message = "\n".join(data)
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=reply_message)
+    )
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000,debug=True)
